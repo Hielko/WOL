dfd258ad9ea6d56f7eaf044aaf60c93da3fc1ca2
Hielko2 <44575202+Hielko2@users.noreply.github.com>
2019-01-06 12:39:41 +0100

    Initial commit

---
+++ WOL/WOL/Index.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WOL.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+     <style type="text/css">
+        .ListBoxCssClass
+        {
+            color:GhostWhite;
+            background-color:DarkOliveGreen;
+            font-family:Courier New;
+            font-size:large;
+            font-style:italic;
+            }
+         #TextArea1 {
+             height: 75px;
+             width: 864px;
+         }
+    </style>
+
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <div>
+            <asp:Label ID="lblTitle" runat="server" Text="Wol Clients"></asp:Label>
+                  </div>
+            <asp:ListBox ID="lbWolClients" runat="server" Width="435px" CssClass="ListBoxCssClass" Height="272px" OnSelectedIndexChanged="lbWolClients_SelectedIndexChanged" ></asp:ListBox>
+             <asp:Panel ID="Panel1" runat="server" Height="49px">
+                 <asp:Button ID="BtnWakeUp" runat="server" Text="Wakeup" OnClick="BtnWakeUp_Click" />
+             </asp:Panel>
+        </div>
+        <asp:Panel ID="Panel2" runat="server" Height="89px">
+            <asp:TextBox ID="TBLog" runat="server" Height="80px" ReadOnly="True" TextMode="MultiLine" Width="1114px"></asp:TextBox>
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ WOL/WOL/Index.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace WOL
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        private WolClients wolClients;
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            wolClients = new WolClients(HttpContext.Current.Server.MapPath("~") + @"wol.xml");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                wolClients.GetMachines.ForEach(machine => lbWolClients.Items.Add(machine.Name));
+            }
+        }
+
+        protected void lbWolClients_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BtnWakeUp.Enabled = lbWolClients.SelectedIndex >= 0;
+        }
+
+        protected void BtnWakeUp_Click(object sender, EventArgs e)
+        {
+            if (lbWolClients.SelectedIndex >= 0)
+            {
+                var Item = lbWolClients.Items[lbWolClients.SelectedIndex];
+                var machine = wolClients.GetMachine(Item.Text);
+                if (machine != null)
+                {
+                    TBLog.Text += $"Waking up {Item.Text}, MAC {machine.MAC} .... ";
+                    try
+                    {
+                        new WOLUtil().SendWOL(machine.MAC);
+                    } catch (Exception ex)
+                    {
+                        TBLog.Text += ex.ToString();
+                    }
+                }
+                else
+                {
+                    TBLog.Text += $"Unknown machine {Item.Text}";
+                }
+
+            }
+
+        }
+    }
+}
